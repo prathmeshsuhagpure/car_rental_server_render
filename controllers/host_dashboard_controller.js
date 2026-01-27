e10a7ea304c7ca655862919922489796b5b8e912
Prathmesh Suhagpure <prathmeshsuhagpure3@gmail.com>
2026-01-27 23:49:54 +0530

    added review schema

--- controllers/host_dashboard_controller.js
+++ controllers/host_dashboard_controller.js
@@ -137,7 +137,8 @@ const getHostDashboard = async (req, res) => {
       totalCars,
       activeRentals,
       monthlyEarnings: monthlyEarnings[0]?.total || 0,
-      rating: ratingData[0]?.avgRating || 0,
+      //rating: ratingData[0]?.avgRating || 0,
+      rating: ratingData,
       cars: carsWithRentalInfo, 
       recentActivities: recentBookings.map((b) => ({
         title: `${b.carId ? `${b.carId.brand} ${b.carId.model}` : "Unknown Car"} booked`,
